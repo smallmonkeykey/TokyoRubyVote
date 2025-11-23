56e0b26fcf85d0898e7d66e357768aa4b43248b6
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-24 00:55:18 +0900

    omniauth-rails_csrf_protectionを削除し、OmniAuth 標準のCSRF保護に移行

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new(key: :_csrf_token)
+
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret]
 end
